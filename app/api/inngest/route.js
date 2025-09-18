ceff54f15b4894cb7e8c5ef0bd461298ea234048
yuumatomiyasu <hououinn0227@gmail.com>
2025-09-19 04:25:09 +0900

    fix:route.jsのエラー修正

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,5 +1,5 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation } from "@/config/inngest";
+import { inngest, syncUserCreation, syncUserDeletion, syneUserUpdation } from "@/config/inngest";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
